7962c33fc6832c5f283107ec938c070adb1782a8
Zaheer Ahmad <zaheerahmadlangrial92@gmail.com>
2024-07-25 16:35:08 +0500

    Learn react native

--- ai-trip-planner/app/(tabs)/mytrip.jsx
+++ ai-trip-planner/app/(tabs)/mytrip.jsx
@@ -4,41 +4,44 @@ import { Colors } from "../../constants/Colors";
 import { Ionicons } from "@expo/vector-icons";
 import StartNewTripCard from "../../components/MyTrips/StartNewTripCard";
 import Trips from "../../components/MyTrips/Trips";
-// import { collection, doc, getDoc, onSnapshot, orderBy, query, QuerySnapshot } from "firebase/firestore";
 import { auth, fireDB } from "../../config/firebase";
-import { collection, getDoc, onSnapshot, orderBy, query } from "firebase/firestore";
+import {
+  collection,
+  getDocs,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default function MyTrip() {
   const user = auth.currentUser;
-  const [userTrips, setUserTrips] = useState();
- 
-
+  const [userTrips, setUserTrips] = useState([]);
 
   useEffect(() => {
-    fetchTrips();
-},[])
+    user && fetchTrips();
+  }, [user]);
 
   const fetchTrips = async () => {
     try {
-      const q = query(collection(fireDB, "trips"), orderBy("time"));
-      const data = onSnapshot(q, (QuerySnapshot) => {
-        console.log('queryShoot', QuerySnapshot)
-        let tripArray = [];
-        QuerySnapshot.forEach((doc) =>
-          tripArray.push({
-            ...doc?.data(),
-            id: doc?.id,
-          })
-        );
-        setUserTrips(tripArray);
+      const q = query(
+        collection(fireDB, "trips"),
+        // orderBy("timestamp", "desc"),
+        // orderBy('time','desc'),
+        where("userId", "==", user?.uid)
+      );
+      const querySnapShot = await getDocs(q);
+      let tripsArr = [];
+      querySnapShot.forEach((doc) => {
+        tripsArr.push({
+          ...doc.data(),
+        });
       });
-      return () => data;
+      console.log("arr", tripsArr);
+      setUserTrips(tripsArr);
     } catch (error) {
       console.log(error);
     }
   };
-
-  console.log('usertrips ==> ', userTrips)
   return (
     <View
       style={{

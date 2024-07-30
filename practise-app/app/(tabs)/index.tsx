9ecfe96ad28f358d8b2c5f22f9521b74aace1719
Zaheer Ahmad <zaheerahmadlangrial92@gmail.com>
2024-07-30 15:37:03 +0500

    Plus and deduct process Done

--- practise-app/app/(tabs)/index.tsx
+++ practise-app/app/(tabs)/index.tsx
@@ -1,44 +1,115 @@
 import {
   StyleSheet,
   Text,
-  Platform,
   View,
   TextInput,
   TouchableOpacity,
   Image,
+  ToastAndroid,
 } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { Colors } from "@/constants/Colors";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import RadioGroup from "react-native-radio-buttons-group";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+type IData = { adnanPrice: number; shahzadPrice: number };
 
 export default function HomeScreen() {
   const [selectedId, setSelectedId] = useState("");
-  const [priceData, setPriceData] = useState({
-    adnanPrice: "300",
-    shahzadPrice: "100",
+  const [priceInput, setPriceInput] = useState("");
+  const [priceData, setPriceData] = useState<IData>({
+    adnanPrice: 0,
+    shahzadPrice: 0,
   });
-  console.log("SelectedId --->", selectedId);
 
   const radioButtons = useMemo(
     () => [
       {
-        id: "1", // acts as primary key, should be unique and non-empty string
-        label: "Malik Adnan",
+        id: "1",
+        label: "Adnan",
         value: "adnanMalik",
       },
       {
         id: "2",
-        label: "Xyz",
+        label: "Shahzad",
         value: "xyz",
       },
     ],
     []
   );
 
-  const handleAddProcess = () => {
-    
-  }
+  const getLocaleStorageData = async () => {
+    const data = await AsyncStorage.getItem("data");
+    if(data !== null) {
+      setPriceData(JSON.parse(data as string));
+    }
+  };
+
+  useEffect(() => {
+    getLocaleStorageData();
+  }, [priceData]);
+
+  const handleAddProcess = async () => {
+    try {
+      if (selectedId === "1") {
+        console.log("Adnan Malik");
+
+        console.log('priceINput value ===>', priceInput)
+        await AsyncStorage.setItem(
+          "data",
+          JSON.stringify({
+            ...priceData,
+            adnanPrice: priceData?.adnanPrice + Number(priceInput),
+          })
+        );
+        
+      } else {
+        await AsyncStorage.setItem(
+          "data",
+          JSON.stringify({
+            ...priceData,
+            shahzadPrice: priceData?.shahzadPrice + Number(priceInput),
+          })
+        );
+      }
+
+      setPriceInput("");
+      ToastAndroid.show('Add Successfully', ToastAndroid.BOTTOM)
+    } catch (error) {
+      console.error("Error saving data:", error);
+      ToastAndroid.show('Something wants wrong', ToastAndroid.TOP);
+    }
+  };
+
+  const handleDeductProcess = async () => {
+    try {
+      if (selectedId === "1") {
+        await AsyncStorage.setItem(
+          "data",
+          JSON.stringify({
+            ...priceData,
+            adnanPrice: priceData?.adnanPrice - Number(priceInput),
+          })
+        );
+        
+      } else {
+        await AsyncStorage.setItem(
+          "data",
+          JSON.stringify({
+            ...priceData,
+            shahzadPrice: priceData?.shahzadPrice - Number(priceInput),
+          })
+        );
+      }
+
+      setPriceInput("");
+      ToastAndroid.show('Deduct Successfully', ToastAndroid.BOTTOM)
+    } catch (error) {
+      console.error("Error saving data:", error);
+      ToastAndroid.show('Something wants wrong', ToastAndroid.TOP);
+    }
+  };
   return (
     <View
       style={{
@@ -63,19 +134,19 @@ export default function HomeScreen() {
       <View style={Styles.personsContainer}>
         <View style={Styles.persons}>
           <Ionicons name="person-outline" size={34} color="black" />
-          <Text style={Styles.personsText}>Malik Adnan</Text>
-          <Text style={Styles.rs}>{priceData.adnanPrice}/-</Text>
+          <Text style={Styles.personsText}>Adnan Bolach</Text>
+          <Text style={Styles.rs}>{priceData?.adnanPrice ?? 0}/-</Text>
         </View>
         <View style={Styles.persons}>
           <Ionicons name="person-outline" size={34} color="black" />
-          <Text style={Styles.personsText}>xyz</Text>
-          <Text style={Styles.rs}>{priceData.shahzadPrice}/-</Text>
+          <Text style={Styles.personsText}>Shahzad Mastoi</Text>
+          <Text style={Styles.rs}>{priceData?.shahzadPrice ?? 0}/-</Text>
         </View>
       </View>
 
       <Image
         source={{
-          uri: 'https://png.pngtree.com/thumb_back/fw800/background/20240707/pngtree-red-glow-banner-background-image_15990915.jpg',
+          uri: "https://png.pngtree.com/thumb_back/fw800/background/20240707/pngtree-red-glow-banner-background-image_15990915.jpg",
         }}
         style={{
           width: "100%",
@@ -90,7 +161,6 @@ export default function HomeScreen() {
           style={{
             marginTop: 20,
             fontSize: 35,
-            // fontWeight: 'bold',
             fontFamily: "outfit-bold",
             color: Colors.orange,
           }}
@@ -108,6 +178,10 @@ export default function HomeScreen() {
             Enter Amount
           </Text>
           <TextInput
+            value={priceInput}
+            onChangeText={(value) => setPriceInput(value)}
+            keyboardType="numeric"
+            placeholder="Amount"
             style={{
               width: "30%",
               borderWidth: 1,
@@ -144,7 +218,7 @@ export default function HomeScreen() {
               Add
             </Text>
           </TouchableOpacity>
-          <TouchableOpacity style={Styles.button}>
+          <TouchableOpacity style={Styles.button} onPress={handleDeductProcess}>
             <Text
               style={{
                 textAlign: "center",
@@ -164,14 +238,12 @@ export default function HomeScreen() {
 
 const Styles = StyleSheet.create({
   personsContainer: {
-    // borderWidth: 1,
     marginTop: 20,
     display: "flex",
     flexDirection: "row",
     justifyContent: "space-between",
   },
   persons: {
-    // borderWidth: 1,
     borderRadius: 16,
     gap: 4,
     width: 170,
@@ -206,9 +278,7 @@ const Styles = StyleSheet.create({
   },
   button: {
     width: "45%",
-    // borderWidth: 1,
     padding: 16,
-    // textAlign: 'center',
     borderRadius: 8,
     backgroundColor: Colors.orange,
   },
